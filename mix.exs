00f2db2688adebd375c4c68ec7740bcca9583042
Byron Alley <byron@byronalley.com>
2023-02-09 19:36:11 -0800

    Be less fussy about Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BasicData.MixProject do
     [
       app: :basic_data,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
